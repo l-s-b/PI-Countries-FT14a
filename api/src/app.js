fb646c285cc72ead5f22ebf2001ad0b132f66811
lucio_sb@yahoo.com.ar <lucio_sb@yahoo.com.ar>
2021-07-13 12:14:24 -0300

    first API successful consumption

--- api/src/app.js
+++ api/src/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const axios = require('axios').default;
 
 require('./db.js');
 
 const server = express();
-
 server.name = 'API';
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+server.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -23,6 +22,14 @@ server.use((req, res, next) => {
 });
 
 server.use('/', routes);
+
+//Routes!
+
+server.get('/countries', (req, res) => {
+  axios.get('https://restcountries.eu/rest/v2/all')
+    .then(response => res.json(response.data))
+    .catch(error => res.status(500).json({error: 'Error 500: Cannot fetch API.'}))
+})
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
